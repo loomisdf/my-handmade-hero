9d2ca8f90af46ef44d863ecbfd007495891010ed
Daltin Loomis <loomisdf@gmail.com>
2016-07-05 20:07:41 -0500

    first real commit

---
+++ HandmadeHero/misc/build.bat
@@ -0,0 +1,6 @@
+@echo off
+
+mkdir ..\..\build
+pushd ..\..\build
+cl -Zi ..\HandmadeHero\code\win32_handmade.cpp
+popd
---
+++ HandmadeHero/misc/shell.bat
@@ -0,0 +1,3 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x64
+set path="C:\Users\loomisdf\Documents\Visual Studio 2013\Projects\HandmadeHero\HandmadeHero\misc";%path%
